f821ff9263126f9710e77479073aae6289f91b31
kennmle <kennmle@gmail.com>
2024-01-16 22:14:02 -0500

    Added files for converting Dropbox Link to Downloadable:

---
+++ js/dropbox-convert.js
@@ -0,0 +1,17 @@
+function dropboxConvert() {
+    let baseLink = $("#dropbox-link").val()
+    if (baseLink.includes("dropbox.com/scl/") && baseLink.includes("dl=0")) {
+        // Replace "www" with "dl" in the URL, keeping the file ID unchanged
+        let modifiedLink = baseLink.replace("www.dropbox.com", "dl.dropbox.com").replace("&dl=0", "");
+
+        // Return the modified link
+        $("#dropbox-download-link").val(modifiedLink)
+    } else {
+        $("#dropbox-download-link").val("Link not valid.")
+    }
+}
+
+$(document).ready(()=>{
+    console.log("ready!")
+    $("#convert").on("click",dropboxConvert)
+})
